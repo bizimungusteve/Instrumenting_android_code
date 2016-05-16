44e29618f46fd892d3fe031f5e6124a023a247aa
bizimungu <bizimungu@localhost.localdomain>
2016-05-16 22:18:21 +0200

    javadoc

--- src/main/java/pji/spoon/methods/OnLongClickHandler.java
+++ src/main/java/pji/spoon/methods/OnLongClickHandler.java
@@ -1,10 +1,10 @@
 package pji.spoon.methods;
 
-public class OnLongClickHandler extends OnEventHandler {
+public class OnLongClickHandler extends ItfOnEventHandler {
 
 	public OnLongClickHandler() {
 		// TODO Auto-generated constructor stub
-		super("onLongClick","android.view.View.OnLongClickListener");
+		super("onLongClick","boolean onLongClick(android.view.View)","android.view.View.OnLongClickListener");
 	}
 
 }
